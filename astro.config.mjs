084b9fc0415db417d83b6b0e347c844cebb80f9b
Michael Lazebny <michael@lazebny.io>
2023-09-08 15:30:59 +0200

    Update for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 import markdoc from "@astrojs/markdoc";
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercelStatic(
+    {
+      analytics: true,
+    }
+  ),
   integrations: [starlight({
     title: 'Sizzle Starter',
     social: {
