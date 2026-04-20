c4528a490da528e1968b526404b590268841d69e
Michael <miskadl09@gmail.com>
2026-04-20 16:53:50 +0200

    upd site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,57 +1,26 @@
-import sitemap from "@astrojs/sitemap";
-import starlight from "@astrojs/starlight";
-import { defineConfig } from "astro/config";
-const site = "https://sizzle.lazebny.io/";
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site,
-  output: "static",
-  integrations: [
-    starlight({
-      title: "Sizzle Starter",
-      social: {
-        github: "https://github.com/hawkkiller/sizzle_starter",
-      },
-      favicon: "/fav.svg",
-      logo: {
-        light: "./src/assets/sizzlelogolight.svg",
-        dark: "./src/assets/sizzlelogodark.svg",
-        replacesTitle: true,
-      },
-      editLink: {
-        baseUrl: "https://github.com/hawkkiller/sizzle_starter_docs/edit/main/",
-      },
-      sidebar: [
-        {
-          label: "Get Started",
-          link: "/get-started"
-        },
-        {
-          label: "App Architecture",
-          autogenerate: {
-            directory: "app-architecture",
-          }
-        }
-      ],
-      head: [
-        {
-          tag: "meta",
-          attrs: {
-            property: "og:image",
-            content: site + "og.jpg?v=1",
-          },
-        },
-        {
-          tag: "meta",
-          attrs: {
-            property: "twitter:image",
-            content: site + "og.jpg?v=1",
-          },
-        },
-      ],
-      customCss: [],
-    }),
-    sitemap(),
-  ],
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
 });
