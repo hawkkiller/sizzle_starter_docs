40116f84556669557142f12ab2217522a08b00bc
Michael <miskadl09@gmail.com>
2026-04-20 17:00:00 +0200

    config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,16 +10,9 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+					label: 'Explanation',
+					autogenerate: { directory: 'explanation' },
+				}
 			],
 		}),
 	],
